2e736df093e09942043a14e6cf80aa89ef38773e
dahalutsab <dlutsab2120@gmail.com>
2025-05-25 21:01:10 +0545

    initial commit

---
+++ bdfs/src/main/java/com/bibek/bdfs/BdfsApplication.java
@@ -0,0 +1,13 @@
+package com.bibek.bdfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdfsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdfsApplication.class, args);
+	}
+
+}
---
+++ bdfs/src/test/java/com/bibek/bdfs/BdfsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bibek.bdfs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdfsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
