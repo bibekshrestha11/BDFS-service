8aca785702e3c90d9159b085a6e019a0269dfe7d
Deejs2 <jireldhiraj123@gmail.com>
2025-06-30 17:39:49 +0545

    implement post controller for bloodrequest

--- bdfs/src/main/java/com/bibek/bdfs/blood_request/controller/BloodRequestController.java
+++ bdfs/src/main/java/com/bibek/bdfs/blood_request/controller/BloodRequestController.java
@@ -1,4 +1,24 @@
 package com.bibek.bdfs.blood_request.controller;
 
-public class BloodRequestController {
+import com.bibek.bdfs.blood_request.dto.BloodRequest;
+import com.bibek.bdfs.blood_request.dto.BloodResponse;
+import com.bibek.bdfs.blood_request.service.BloodRequestService;
+import com.bibek.bdfs.common.BaseController;
+import com.bibek.bdfs.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/blood-requests")
+public class BloodRequestController extends BaseController {
+    private final BloodRequestService bloodRequestService;
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<BloodResponse>> createBloodRequest(BloodRequest bloodRequest) {
+        return successResponse(bloodRequestService.createBloodRequest(bloodRequest), "Blood request created successfully");
+    }
 }
