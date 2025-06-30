c41967945e1b19b637740695cf6149912c761899
Deejs2 <jireldhiraj123@gmail.com>
2025-06-30 16:44:01 +0545

    implement required api for the blood requests

--- bdfs/src/main/java/com/bibek/bdfs/blood_request/dto/BloodResponse.java
+++ bdfs/src/main/java/com/bibek/bdfs/blood_request/dto/BloodResponse.java
@@ -1,4 +1,37 @@
 package com.bibek.bdfs.blood_request.dto;
 
+import com.bibek.bdfs.blood_request.entity.BloodRequestEntity;
+import com.bibek.bdfs.blood_request.entity.BloodRequestStatus;
+import com.bibek.bdfs.blood_request.entity.UrgencyLevel;
+import com.bibek.bdfs.user.entity.BloodGroup;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class BloodResponse {
+    private BloodRequestUserResponse user;
+    private BloodGroup bloodTypeNeeded;
+    private int quantity;
+    private UrgencyLevel urgencyLevel;
+    private double latitude;
+    private double longitude;
+    private String hospitalName;
+    private String hospitalAddress;
+    private String additionalNotes;
+    private BloodRequestStatus status;
+    private String expiresAt;
+
+    public BloodResponse(BloodRequestEntity bloodRequestEntity){
+        this.bloodTypeNeeded = bloodRequestEntity.getBloodTypeNeeded();
+        this.quantity = bloodRequestEntity.getQuantity();
+        this.urgencyLevel = bloodRequestEntity.getUrgencyLevel();
+        this.latitude = bloodRequestEntity.getLatitude();
+        this.longitude = bloodRequestEntity.getLongitude();
+        this.hospitalName = bloodRequestEntity.getHospitalName();
+        this.hospitalAddress = bloodRequestEntity.getHospitalAddress();
+        this.additionalNotes = bloodRequestEntity.getAdditionalNotes();
+        this.status = bloodRequestEntity.getStatus();
+        this.expiresAt = bloodRequestEntity.getExpiresAt();
+    }
 }
--- bdfs/src/main/java/com/bibek/bdfs/blood_request/service/BloodRequestService.java
+++ bdfs/src/main/java/com/bibek/bdfs/blood_request/service/BloodRequestService.java
@@ -1,4 +1,18 @@
 package com.bibek.bdfs.blood_request.service;
 
+import com.bibek.bdfs.blood_request.dto.BloodRequest;
+import com.bibek.bdfs.blood_request.dto.BloodResponse;
+import com.bibek.bdfs.blood_request.entity.UrgencyLevel;
+import com.bibek.bdfs.user.entity.BloodGroup;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface BloodRequestService {
+    BloodResponse createBloodRequest(BloodRequest bloodRequest);
+    BloodResponse getBloodRequestById(Long id);
+    BloodResponse updateBloodRequest(Long id, BloodRequest bloodRequest);
+    void deleteBloodRequest(Long id);
+    Page<BloodResponse> getAllBloodRequests(Pageable pageable);
+    Page<BloodResponse> getBloodRequestsByLoggedInUser(Pageable pageable);
+    Page<BloodResponse> getBloodRequestsByBloodGroup(BloodGroup bloodGroup, UrgencyLevel urgencyLevel, Pageable pageable);
 }
