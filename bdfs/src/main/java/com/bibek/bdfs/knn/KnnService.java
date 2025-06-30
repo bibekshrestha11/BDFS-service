aec5c65941c3164e4fe8fff1784a2f38f2d67584
Pasang60 <pasanggelbu@gmail.com>
2025-06-30 16:44:26 +0545

    knn algorithm implemented

--- bdfs/src/main/java/com/bibek/bdfs/knn/KnnService.java
+++ bdfs/src/main/java/com/bibek/bdfs/knn/KnnService.java
@@ -1,4 +1,10 @@
 package com.bibek.bdfs.knn;
 
+import com.bibek.bdfs.blood_request.entity.BloodRequest;
+import com.bibek.bdfs.user.entity.User;
+
+import java.util.List;
+
 public interface KnnService {
+    List<User> findNearestDonors(BloodRequest request);
 }
